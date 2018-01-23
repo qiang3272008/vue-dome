353870a27ab7ae68a8ff781db5d49c9e61ae1fad
jianglin521 <17610835880@163.com>
2018-01-23 12:01:16 +0800

    11

--- src/components/nav/nav.vue
+++ src/components/nav/nav.vue
@@ -1,101 +1,46 @@
 <template>
-  <el-row class="container">
-    <el-col :span="24" class="main">
-      <!--导航菜单-->
-      <el-menu :default-active="$route.path" class="el-menu-vertical-demo" @open="handleopen" @close="handleclose" @select="handleselect"
-               unique-opened router v-show="!collapsed">
-        <template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
-          <el-submenu :index="index+''" v-if="!item.leaf">
-            <template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
-            <el-menu-item v-for="child in item.children" :index="child.path" :key="child.path" v-if="!child.hidden">{{child.name}}</el-menu-item>
-          </el-submenu>
-          <el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path"><i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
+  <el-col :span="12">
+    <h5>默认颜色</h5>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
         </template>
-      </el-menu>
-      <!--导航菜单-折叠后-->
-      <ul class="el-menu el-menu-vertical-demo collapsed" v-show="collapsed" ref="menuCollapsed">
-        <li v-for="(item,index) in $router.options.routes" v-if="!item.hidden" class="el-submenu item">
-          <template v-if="!item.leaf">
-            <div class="el-submenu__title" style="padding-left: 20px;" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)"><i :class="item.iconCls"></i></div>
-            <ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)">
-              <li v-for="child in item.children" v-if="!child.hidden" :key="child.path" class="el-menu-item" style="padding-left: 40px;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">{{child.name}}</li>
-            </ul>
-          </template>
-          <template v-else>
-            <li class="el-submenu">
-              <div class="el-submenu__title el-menu-item" style="padding-left: 20px;height: 56px;line-height: 56px;padding: 0 20px;" :class="$route.path==item.children[0].path?'is-active':''" @click="$router.push(item.children[0].path)"><i :class="item.iconCls"></i></div>
-            </li>
-          </template>
-        </li>
-      </ul>
-    </el-col>
-  </el-row>
+        <el-menu-item-group>
+          <template slot="title">分组一</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+    </el-menu>
+  </el-col>
+  <el-col :span="12">
 </template>
 
 <script>
   export default {
-    data() {
-      return {
-        sysName:'VUEADMIN',
-        collapsed:false,
-        sysUserName: '',
-        sysUserAvatar: '',
-        form: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        }
-      }
-    },
-    methods: {
-      onSubmit() {
-        console.log('submit!');
-      },
-      handleopen() {
-        //console.log('handleopen');
-      },
-      handleclose() {
-        //console.log('handleclose');
-      },
-      handleselect: function (a, b) {
-      },
-      //退出登录
-      logout: function () {
-        var _this = this;
-        this.$confirm('确认退出吗?', '提示', {
-          //type: 'warning'
-        }).then(() => {
-          sessionStorage.removeItem('user');
-          _this.$router.push('/login');
-        }).catch(() => {
-
-        });
-
-
-      },
-      //折叠导航栏
-      collapse:function(){
-        this.collapsed=!this.collapsed;
-      },
-      showMenu(i,status){
-        this.$refs.menuCollapsed.getElementsByClassName('submenu-hook-'+i)[0].style.display=status?'block':'none';
-      }
-    },
-    mounted() {
-      var user = sessionStorage.getItem('user');
-      if (user) {
-        user = JSON.parse(user);
-        this.sysUserName = user.name || '';
-        this.sysUserAvatar = user.avatar || '';
-      }
-
-    }
   }
+
 
 </script>
 
